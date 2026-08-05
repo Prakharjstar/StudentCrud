5c02c21d45e588931f221bdb5a54069edd10c8a6
Prakhar <prakharjoshi960@gmail.com>
2026-08-05 18:11:35 +0530

    first commit

---
+++ CrudSpringBoot/src/main/java/com/Student/CrudSpringBoot/CrudSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.Student.CrudSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootApplication.class, args);
+		System.out.print("Hello I am Prakhar");
+	}
+
+}
